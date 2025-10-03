db40dcff93f6d47054b7aef75e424be080467790
happy kumar <happy@gmail.com>
2025-10-03 23:51:18 +0530

    new commite is added like uncommit function in video.jsx

--- src/component/Video.jsx
+++ src/component/Video.jsx
@@ -2,16 +2,15 @@ import { Fragment } from "react"
 import { useState } from "react";
 
 function Video({channel,title,time,url,keys}) {
-  // function handleClick(e) {console.log("Clicked on",e.target.className),(e.target).style.color="red";}
+ function handleClick(e) {console.log("Clicked on",e.target.className),(e.target).style.color="red";}
 
- const [number, setNumber] = useState(0)
-function count() {setNumber(number + 10)}
+
   return (
      <div>
-      <h1>{number}</h1>
-      <button onClick={count}>counter</button>
-     {/* <div className="raper">
-     <iframe src={url} className="img" frameborder="0" key={keys}></iframe></div><br/><button className="button"><a href={url}>Subscribe</a></button><br/> channel {channel}<br/><button className="btn" onClick={handleClick}>addbtn</button>title {title}<br/>Time {time} */}
+      
+      
+     <div className="raper"><br />
+     <iframe src={url} className="img" frameborder="0" key={keys}></iframe></div><br/><button className="button"><a href={url}>Subscribe</a></button><br/> channel {channel}<br/>Title {title}<br/>Time {time}
      </div>
 
   )
